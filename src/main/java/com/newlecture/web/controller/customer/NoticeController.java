2512c87702bcac5640b2bc7527288e0e7da7933f
admin <admin@DESKTOP-OOKDBJ5>
2022-06-08 01:18:13 +0900

    newlecture spring boot ep7

---
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -0,0 +1,23 @@
+package com.newlecture.web.controller.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoticeController {
+
+	@RequestMapping("/customer/notice/list")
+	public String list(Model model) {
+		
+		model.addAttribute("test", "Hello~");
+		
+		return "list.jsp";
+	}
+	
+	@RequestMapping("/customer/notice/detail")
+	public String detail() {
+		return "detail.jsp";
+	}
+	
+}
